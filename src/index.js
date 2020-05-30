b900de37970d89c88569acafcb2078ec66cc026c
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-05-29 21:43:35 -0700

    Removed test runner js file from src

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { getRank } from "./draw_poker_functions";
 import { analyzeHand } from "./draw_poker_functions";
 import { runDrawPoker } from "./draw_poker_runner";
-import { testRunDrawPoker } from "./test_draw_poker_runner";
+
 
 
 window.addEventListener('load', function () {
     // setTimeout(() => {  runDrawPoker(); }, 2000);
 
     
-    testRunDrawPoker();
+    runDrawPoker();
 
 });
