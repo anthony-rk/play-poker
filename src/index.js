93851c8de16bd63a1021debcd93a171efc58b118
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-06-09 13:13:00 -0700

    Add printGreeting function to be displayed in the Console for the game rules / rewards

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './index.css';
 
 import { pageInitializer } from "./page_initializer";
+import { printGreeting } from "./draw_poker_functions";
 
 window.addEventListener('load', function () {
     pageInitializer();
-
+	printGreeting();
 });
